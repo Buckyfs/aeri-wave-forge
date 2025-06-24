5f60215ecc21d974ade83fba81f66cd0425b8b39
Buckyfs <prophyt@gmail.com>
2025-06-24 16:40:11 -0400

    Update pagination.tsx

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,4 +1,12 @@
-import * as React from "react"
+import {
+  forwardRef,
+  useRef,
+  useEffect,
+  useState,
+  ElementRef,
+  ComponentPropsWithoutRef
+} from "react";
+
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
 import { cn } from "@/lib/utils"
