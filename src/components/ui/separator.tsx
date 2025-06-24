23056aeff9b549cb84ff82dc78638c3923cf8689
Buckyfs <prophyt@gmail.com>
2025-06-24 16:28:57 -0400

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,13 @@
-import * as React from "react"
+import {
+  forwardRef,
+  useRef,
+  useEffect,
+  useState,
+  ElementRef,
+  ComponentPropsWithoutRef
+} from "react";
+
+
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
