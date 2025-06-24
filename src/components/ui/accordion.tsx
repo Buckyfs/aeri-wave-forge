4e78ca311e36bb03752d6e13499a83c50c25e5c9
Buckyfs <prophyt@gmail.com>
2025-06-24 16:45:10 -0400

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,4 +1,12 @@
-import * as React from "react"
+import {
+  forwardRef,
+  useRef,
+  useEffect,
+  useState,
+  ElementRef,
+  ComponentPropsWithoutRef
+} from "react";
+
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
 
